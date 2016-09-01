9f2449077fb337d576a860743eb120af4365f384
王国平 <@WANG>
2016-09-01 10:27:05 +0800

    添加项目文件。

---
+++ SportsStore.WebUI/Infrastructure/NinjiectDependencyResolver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Web.Mvc;
+using Ninject;
+using Moq;
+using SportsStore.Domain.Concrete;
+
+
+namespace SportsStore.WebUI.Infractructure
+{
+    public class NinjectDependcyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependcyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddiBindings();
+        }
+        private void AddiBindings()
+        {
+            kernel.Bind<IProductsRepository>().To<EFProductRepository>();
+            //Mock<IProductsRepository> mock = new Mock<IProductsRepository>();
+            //mock.Setup(m => m.Products).Returns(new List<Product>
+            //{
+            //    new Product {Name="Football",Price=25 },
+            //    new Product {Name="Surf board",Price=179 },
+            //    new Product {Name="Running shoes",Price=95 }
+            //});
+            //kernel.Bind<IProductsRepository>().ToConstant(mock.Object);
+
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+    }
+}
